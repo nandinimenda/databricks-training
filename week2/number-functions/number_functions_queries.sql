49475d0f71256d917a3f9838a36e2b384830a9c7
nandinimenda <160224691+nandinimenda@users.noreply.github.com>
2026-05-17 20:40:11 +0530

    Add SQL queries for various number functions

--- week2/number-functions/number_functions_queries.sql
+++ week2/number-functions/number_functions_queries.sql
@@ -1,13 +1,56 @@
+-- 1. ABS – Absolute Value
 SELECT ABS(-100);
 
-SELECT ROUND(456.789,2);
+-- 2. ROUND – Round Salary to 0 Decimals
+SELECT ROUND(base_salary, 0)
+FROM employee_salary;
 
-SELECT CEIL(45.12);
+-- 3. ROUND – Round Salary to 2 Decimals
+SELECT ROUND(base_salary, 2)
+FROM employee_salary;
 
-SELECT FLOOR(45.99);
+-- 4. CEIL – Round Up
+SELECT CEIL(base_salary)
+FROM employee_salary;
 
-SELECT MOD(10,3);
+-- 5. FLOOR – Round Down
+SELECT FLOOR(base_salary)
+FROM employee_salary;
 
-SELECT POWER(2,3);
+-- 6. TRUNCATE – Cut Decimals
+SELECT TRUNCATE(base_salary, 1)
+FROM employee_salary;
 
+-- 7. MOD – Find Remainder
+SELECT MOD(experience_years, 2)
+FROM employee_salary;
+
+-- 8. POWER Function
+SELECT POWER(2, 3);
+
+-- 9. POW Function
+SELECT POW(5, 2);
+
+-- 10. SQRT – Square Root
 SELECT SQRT(64);
+
+-- 11. SIGN – Sign of Salary
+SELECT SIGN(base_salary)
+FROM employee_salary;
+
+-- 12. RAND – Random Number
+SELECT RAND();
+
+-- 13. FORMAT – Format Salary
+SELECT FORMAT(base_salary, 2)
+FROM employee_salary;
+
+-- 14. GREATEST – Maximum Value
+SELECT emp_name,
+GREATEST(base_salary, IFNULL(bonus, 0))
+FROM employee_salary;
+
+-- 15. LEAST – Minimum Value
+SELECT emp_name,
+LEAST(base_salary, IFNULL(bonus, 0))
+FROM employee_salary;
