60e2ceaee2d2620e4cf14c3a23cfe632d8a92131
nandinimenda <160224691+nandinimenda@users.noreply.github.com>
2026-05-18 08:34:32 +0530

    Add SQL queries for employee data transformations
    Added SQL queries to manipulate employee names and calculate income.

--- week2/string-data-number/string_data_number_queries.sql
+++ week2/string-data-number/string_data_number_queries.sql
@@ -1,14 +1,38 @@
+-- 1. Convert emp_name to uppercase
 SELECT UPPER(emp_name)
 FROM employee_payments;
 
+-- 2. Convert emp_name to lowercase
 SELECT LOWER(emp_name)
 FROM employee_payments;
 
-SELECT ROUND(base_salary,0)
+-- 3. Convert emp_name to Proper Case
+SELECT CONCAT(
+UPPER(LEFT(emp_name,1)),
+LOWER(SUBSTRING(emp_name,2))
+)
 FROM employee_payments;
 
-SELECT CONCAT(emp_name,' - ',department)
+-- 4. Calculate total income
+SELECT emp_name,
+base_salary + IFNULL(bonus,0) AS total_income
 FROM employee_payments;
 
+-- 5. Round total income
+SELECT ROUND(base_salary + IFNULL(bonus,0),0)
+FROM employee_payments;
+
+-- 6. Extract joining year
 SELECT YEAR(joining_date)
+FROM employee_payments;
+
+-- 7. Employee Classification
+SELECT emp_name,
+CASE
+WHEN TIMESTAMPDIFF(YEAR, joining_date, CURDATE()) > 7
+THEN 'Senior'
+WHEN TIMESTAMPDIFF(YEAR, joining_date, CURDATE()) BETWEEN 4 AND 7
+THEN 'Mid'
+ELSE 'Junior'
+END AS employee_level
 FROM employee_payments;
