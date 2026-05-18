06a43e619e4c7299ff12995ce66c630069e07203
nandinimenda <160224691+nandinimenda@users.noreply.github.com>
2026-05-18 08:12:22 +0530

    Add regex_practice table and sample data
    Created a new table 'regex_practice' and inserted sample data for regex testing.

--- week2/regex/regex_assignment.sql
+++ week2/regex/regex_assignment.sql
@@ -1,9 +1,18 @@
-SELECT REGEXP_SUBSTR('abc123xyz', '[0-9]+');
+CREATE TABLE regex_practice (
+id INT,
+full_text VARCHAR(200),
+email VARCHAR(100),
+phone VARCHAR(30),
+mixed_value VARCHAR(100)
+);
 
-SELECT REGEXP_SUBSTR('karthik@gmail.com', '@[a-zA-Z.]+');
+INSERT INTO regex_practice VALUES
+(1,'EMP001_IN_91','karthik@gmail.com','+91-9876543210','abc123xyz'),
 
-SELECT REGEXP_SUBSTR('+91-9876543210', '[0-9]{10}');
+(2,'EMP002_US_01','rahul@yahoo.com','+1-7654321098','test45done'),
 
-SELECT REGEXP_REPLACE('abc123', '[0-9]', '');
+(3,'EMP003_UK_44','john@outlook.com','+44-9988776655','run9fast'),
 
-SELECT 'gmail.com' REGEXP '[a-z]+';
+(4,'EMP004_AU_61','steve@gmail.com','+61-8899776655','go88home'),
+
+(5,'EMP005_IN_91','arjun@company.in','+91-7788996655','data77lake');
