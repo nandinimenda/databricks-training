3beb78f4129c54952d6ae18512ad1791b045e133
nandinimenda <160224691+nandinimenda@users.noreply.github.com>
2026-05-18 21:47:23 +0530

    Refactor regex queries to use WHERE with REGEXP
    Updated SQL queries to use WHERE clause with REGEXP for various patterns instead of REGEXP_SUBSTR.

--- week2/regex/regex_queries.sql
+++ week2/regex/regex_queries.sql
@@ -1,53 +1,116 @@
 -- 1. Extract digits from mixed value
-SELECT REGEXP_SUBSTR(mixed_value, '[0-9]+')
-FROM regex_practice;
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[0-9]';
 
 -- 2. Extract alphabets from mixed value
-SELECT REGEXP_SUBSTR(mixed_value, '[a-zA-Z]+')
-FROM regex_practice;
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[a-zA-Z]';
 
 -- 3. Extract starting digits
-SELECT REGEXP_SUBSTR(mixed_value, '^[0-9]+')
-FROM regex_practice;
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '^[0-9]';
 
 -- 4. Extract ending digits
-SELECT REGEXP_SUBSTR(mixed_value, '[0-9]+$')
-FROM regex_practice;
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[0-9]$';
 
 -- 5. Extract exactly two digits
-SELECT REGEXP_SUBSTR(mixed_value, '[0-9]{2}')
-FROM regex_practice;
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[0-9][0-9]';
 
 -- 6. Extract exactly one digit
-SELECT REGEXP_SUBSTR(mixed_value, '[0-9]')
-FROM regex_practice;
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[0-9]';
 
 -- 7. Extract country code
-SELECT REGEXP_SUBSTR(phone, '[0-9]{2}')
-FROM regex_practice;
 
--- 8. Extract username before @
-SELECT REGEXP_SUBSTR(email, '^[a-zA-Z0-9._-]+')
-FROM regex_practice;
+SELECT phone
+FROM regex_practice
+WHERE phone REGEXP '^\\+[0-9][0-9]';
 
--- 9. Extract domain with @
-SELECT REGEXP_SUBSTR(email, '@[a-zA-Z.]+')
-FROM regex_practice;
+-- 8. Extract numeric portion between alphabets
 
--- 10. Extract domain without @
-SELECT REPLACE(
-REGEXP_SUBSTR(email, '@[a-zA-Z.]+'),
-'@',
-''
-)
-FROM regex_practice;
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[a-zA-Z][0-9]+[a-zA-Z]';
 
--- 11. Extract extension
-SELECT REGEXP_SUBSTR(email, '\\.[a-zA-Z]+$')
-FROM regex_practice;
+-- 9. Extract username before @
 
- -- 12. Email Validation
 SELECT email
 FROM regex_practice
-WHERE email REGEXP
-'[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,3}';
+WHERE email REGEXP '^[a-zA-Z0-9._-]+@';
+
+-- 10. Extract domain with @
+
+SELECT email
+FROM regex_practice
+WHERE email REGEXP '@[a-zA-Z.]';
+
+-- 11. Extract domain without @
+
+SELECT REPLACE(email,'@',' ')
+FROM regex_practice;
+
+-- 12. Extract extension
+
+SELECT email
+FROM regex_practice
+WHERE email REGEXP '\\.[a-zA-Z]+$';
+
+-- 13. Extract alphabets continuously
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[a-zA-Z]+';
+
+-- 14. Extract numbers continuously
+
+SELECT mixed_value
+FROM regex_practice
+WHERE mixed_value REGEXP '[0-9]+';
+
+-- 15. Extract first three characters
+
+SELECT LEFT(full_text,3)
+FROM regex_practice;
+
+-- 16. Extract last two characters
+
+SELECT RIGHT(full_text,2)
+FROM regex_practice;
+
+-- 17. Extract employee number
+
+SELECT full_text
+FROM regex_practice
+WHERE full_text REGEXP '[0-9]+';
+
+-- 18. Extract ending country code
+
+SELECT full_text
+FROM regex_practice
+WHERE full_text REGEXP '[0-9]+$';
+
+-- 19. Extract text between underscores
+
+SELECT full_text
+FROM regex_practice
+WHERE full_text REGEXP '_[A-Z]+_';
+
+-- 20. Extract digits after +
+
+SELECT phone
+FROM regex_practice
+WHERE phone REGEXP '^\\+[0-9][0-9]';
